3628ff34958fce1704d41d9e4bfce37c7f32c8d9
hung1001 <hoanghung96cs@gmail.com>
2020-02-16 14:42:50 +0700

    Initial

---
+++ assets/js/main.js
@@ -0,0 +1,160 @@
+$.get('assets/json/songs.json', function(data) {
+  var song, init = true,
+    tracker = $('.tracker'),
+    volume = $('.js-volume'),
+    randImg = ['avatar-1.jpg', 'avatar-2.jpg', 'avatar-3.jpg', 'avatar-4.jpg', 'avatar-5.jpg', 'avatar-6.jpg', 'avatar-7.jpg'];
+
+  $('.js-playlist-total').text(data.length);
+  data.forEach(function(item) {
+    $('.js-playlist').append('<li data-url="' + item.url + '">' + item.title + '</li>')
+  });
+
+  initAudio($('.js-playlist li:first-child'));
+
+  function initAudio(elem) {
+    var url = elem.attr('data-url'),
+      title = elem.text(),
+      artist = 'Hung1001';
+
+    $('.js-title').text(title);
+    $('.js-artist').text(artist);
+    if (!init) {
+      $('.js-track-thumb-img').attr('src', 'assets/img/' + randImg[Math.floor(Math.random() * randImg.length)]);
+    }
+    song = new Audio(url);
+    song.addEventListener('timeupdate', function() {
+      var curtime = parseInt(song.currentTime, 10),
+        timeLeft = parseInt(song.duration) - curtime,
+        sUp = parseInt(song.currentTime % 60),
+        mUp = parseInt((song.currentTime / 60) % 60),
+        sDown = timeLeft % 60,
+        mDown = Math.floor(timeLeft / 60) % 60;
+      sDown = sDown < 10 ? '0' + sDown : sDown;
+      mDown = mDown < 10 ? '0' + mDown : mDown;
+
+      if (sUp < 10) {
+        $('.js-duration-countup').html(mUp + ':0' + sUp);
+      } else {
+        $('.js-duration-countup').html(mUp + ':' + sUp);
+      }
+
+      $('.js-duration-coundown').html(mDown + ":" + sDown);
+      tracker.slider('value', curtime);
+    });
+
+    $('.js-playlist li').removeClass('active');
+    elem.addClass('active');
+  }
+
+  function playAudio() {
+    song.play();
+    tracker.slider('option', 'max', song.duration);
+    $('.js-play').addClass('d-none');
+    $('.js-pause').addClass('d-block');
+  }
+
+  function stopAudio() {
+    song.pause();
+    $('.js-play').removeClass('d-none');
+    $('.js-pause').removeClass('d-block');
+  }
+
+  $('.js-play').on('click', function(e) {
+    e.preventDefault();
+    $('.js-track-thumb').addClass('play');
+    song.addEventListener('ended', function() {
+      var next = $('.js-playlist li.active').next();
+      if (next.length == 0) {
+        next = $('.js-playlist li:first-child');
+      }
+      initAudio(next);
+      song.addEventListener('loadedmetadata', function() {
+        playAudio();
+      });
+    }, false);
+    tracker.slider('option', 'max', song.duration);
+    song.play();
+    $('.js-play').addClass('d-none');
+    $('.js-pause').addClass('d-block');
+  });
+
+  $('.js-pause').on('click', function(e) {
+    e.preventDefault();
+    stopAudio();
+    $('.js-track-thumb').removeClass('play');
+  });
+
+  $('.js-next').click(function(e) {
+    e.preventDefault();
+    stopAudio();
+    init = false;
+    var next = $('.js-playlist li.active').next();
+    if (next.length === 0) {
+      next = $('.js-playlist li:first-child');
+    }
+    initAudio(next);
+    song.addEventListener('loadedmetadata', function() {
+      playAudio();
+      $('.js-track-thumb').addClass('play');
+    });
+  });
+
+  $('.js-prev').click(function(e) {
+    e.preventDefault();
+    stopAudio();
+    init = false;
+    var prev = $('.js-playlist li.active').prev();
+    if (prev.length === 0) {
+      prev = $('.js-playlist li:last-child');
+    }
+    initAudio(prev);
+    song.addEventListener('loadedmetadata', function() {
+      playAudio();
+      $('.js-track-thumb').addClass('play');
+    });
+  });
+
+  $('.js-playlist li').on('click', function() {
+    stopAudio();
+    initAudio($(this));
+    init = false;
+    song.addEventListener('loadedmetadata', function() {
+      playAudio();
+      $('.js-track-thumb').addClass('play');
+    });
+  });
+
+  song.volume = 0.8;
+  volume.slider({
+    orientation: 'vertical',
+    range: 'max',
+    max: 100,
+    min: 1,
+    value: 100,
+    slide: function(event, ui) {
+      song.volume = ui.value / 100;
+    }
+  });
+
+  tracker.slider({
+    range: 'min',
+    min: 0,
+    max: 10,
+    slide: function(event, ui) {
+      song.currentTime = ui.value;
+    }
+  });
+
+  $('.js-volume-toggle').on('click', function(e) {
+    e.stopPropagation();
+    $('.js-volume').toggleClass('show');
+  });
+
+  $('.js-volume').on('click', function(e) {
+    e.stopPropagation();
+  });
+
+  $('body').on('click', function() {
+    $('.js-volume').removeClass('show');
+  });
+});
